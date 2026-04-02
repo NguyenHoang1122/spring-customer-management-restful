3b8b43ff547ceaf0de5a37ac56265750bf758e6b
NguyenHoang1122 <nguyenhoang9xbn@gmail.com>
2026-04-02 14:27:19 +0700

    update repository and service

--- src/main/java/com/springcustomermanagementrestful/service/CustomerService.java
+++ src/main/java/com/springcustomermanagementrestful/service/CustomerService.java
@@ -1,4 +1,35 @@
 package com.springcustomermanagementrestful.service;
 
-public class CustomerService {
+import com.springcustomermanagementrestful.model.Customer;
+import com.springcustomermanagementrestful.repository.ICustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+@Service
+public class CustomerService implements ICustomerService {
+
+    @Autowired
+    private ICustomerRepository iCustomerRepository;
+
+
+    @Override
+    public Iterable<Customer> findAll() {
+        return null;
+    }
+
+    @Override
+    public Optional<Customer> findById(Long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Customer save(Customer customer) {
+        return null;
+    }
+
+    @Override
+    public void remove(Long id) {
+
+    }
 }
--- src/main/java/com/springcustomermanagementrestful/service/ICustomerService.java
+++ src/main/java/com/springcustomermanagementrestful/service/ICustomerService.java
@@ -1,4 +1,12 @@
 package com.springcustomermanagementrestful.service;
 
+import com.springcustomermanagementrestful.model.Customer;
+
+import java.util.Optional;
+
 public interface ICustomerService {
+    Iterable<Customer> findAll();
+    Optional<Customer> findById(Long id);
+    Customer save(Customer customer);
+    void remove(Long id);
 }
